888d9e9a70cfd8bf5451abcca3af7debc9b1880f
Arne Munthe-Kaas <45269747+h593267@users.noreply.github.com>
2022-12-16 11:19:16 +0100

    remove jpa default logging, add log4j2 to temperature rest controller, add get method for single reading given ID

--- src/main/java/no/arnemunthekaas/smarthome/controller/TemperatureController.java
+++ src/main/java/no/arnemunthekaas/smarthome/controller/TemperatureController.java
@@ -5,25 +5,46 @@ import lombok.extern.log4j.Log4j2;
 import no.arnemunthekaas.smarthome.db.TemperatureDAO;
 import no.arnemunthekaas.smarthome.model.TemperatureReading;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.sql.Timestamp;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("temperature")
 @Log4j2
 public class TemperatureController {
 
+    private final Gson gson = new Gson();
     @Autowired
     private TemperatureDAO temperatureDAO;
-    private final Gson gson = new Gson();
 
     @GetMapping(produces = "application/json")
-    public String findAll() {
+    public String selectAll() {
         List<TemperatureReading> readings = temperatureDAO.findAll();
+        log.info("Select all temperature readings from database: " + readings);
         return gson.toJson(readings);
+    }
+
+    @GetMapping(value = "/{ID}", produces = "application/json")
+    public String select(@PathVariable int ID) {
+        Optional<TemperatureReading> reading = temperatureDAO.findById(ID);
+        log.info("Select temperature reading " + ID + " from database: " + reading);
+
+        if(reading.isEmpty()) {
+            log.warn("Temperature reading " + ID + " not found");
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "Temperature reading " + ID + " not found"
+            );
+        }
+
+        TemperatureReading temperatureReading = reading.get();
+
+        log.info("Temperature reading " + ID + " :" + temperatureReading);
+        return gson.toJson(temperatureReading);
     }
 
     @PostMapping()
@@ -32,9 +53,11 @@ public class TemperatureController {
 
         temperatureReading.setINDOORTEMP(INDOORTEMP);
         temperatureReading.setOUTDOORTEMP(OUTDOORTEMP);
-        temperatureReading.setTIME_STAMP(new Timestamp(System.currentTimeMillis()));
+        temperatureReading.setTIME_STAMP(new Timestamp(System.currentTimeMillis())); // TODO: Should this be sent by arduino (arduino recieves via api -call)
+        log.info("Create new temperature reading: " + temperatureReading);
 
         temperatureDAO.save(temperatureReading);
+        log.info("Insert temperature reading to database: " + temperatureReading);
     }
 
 
